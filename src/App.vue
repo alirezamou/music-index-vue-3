45eb7ab66f463b6f547c9576aa98db36272bc6c2
alirezamou <am808036@gmail.com>
2023-03-21 20:41:37 +0330

    replace MusicIndex comp with router-view

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <v-app>
     <v-main>
-      <MusicIndex />
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import MusicIndex from "@/components/MusicIndex.vue";
 import "@/assets/app.css";
 
 export default {
-  components: {
-    MusicIndex,
-  }
 }
 </script>
 
